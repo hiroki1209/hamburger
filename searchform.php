e0ba9553c6cfd09cc2310d6b5010c72fe9c805ef
hiroki <finalfantasywww@yahoo.co.jp>
2020-12-20 00:05:36 +0900

    form

---
+++ searchform.php
@@ -0,0 +1,4 @@
+<form class="c-Search-form" action="<?php echo home_url( '/' ); ?>" role="search" method="get">
+		<input class="c-search-box" type="search" name="s" value="<?php echo get_search_query(); ?>"></imput>
+		<input class="c-search-btn" type="submit" value="検索"></imput>
+	  </form>

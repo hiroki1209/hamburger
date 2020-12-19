849e32ecbf865176499130ed662e45ca28cd0d17
hiroki <finalfantasywww@yahoo.co.jp>
2020-12-19 23:49:13 +0900

    footer

---
+++ footer.php
@@ -0,0 +1,9 @@
+<footer class="l-footer l-justify-center">
+	<div class="l-footer-nav l-direction-column"> 
+	<div class="l-footer-link l-justify-center"> 
+		<a href="<?php the_permalink( 10 ); ?>">ショップ情報|</a>
+		<a href="<?php the_permalink( 79 ); ?>">ヒストリー</a>
+	</div>
+	<p>Company RaiseTech</p>
+  </div> 
+</footer>  

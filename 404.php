9269e970f10497b6da61af634e1923d1c7f44361
hiroki <finalfantasywww@yahoo.co.jp>
2020-12-20 11:10:22 +0900

    404

---
+++ 404.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta charset="utf-8" />
+<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+<meta name="format-detection" content="telephone=no" />
+<meta name="viewport" content="width=device-width, initial-scale=1" />    
+<meta name="description" content="ハンバーガーサイト" />
+<?php wp_head(); ?>
+</head>
+<div class="404">
+<h2>404 Not Found（ページが見つかりませんでした）</h2>
+  <p>指定された以下のページは存在しないか、または移動した可能性があります。</p>
+</div>
